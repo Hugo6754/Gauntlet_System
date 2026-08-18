39adb2d88834ea1e1e6adc9b7f8bf6dc4307aac5
James <balljames05@gmail.com>
2026-08-18 20:43:43 +0200

    Added new Exceptions to the files

---
+++ Gauntlet_System/Classes/PlayerRecord.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gauntlet_System.Classes
+{
+    public class PlayerRecord
+    {
+        public string Type { get; set; }
+        public string Username { get; set; }
+        public string Nationality { get; set; }
+        public int Elo { get; set; }
+        public int Winstreak { get; set; }
+        public bool Isactive { get; set; }
+    }
+}
